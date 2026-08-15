9b260fb430f269cb1f184d98d616445cf161ce9a
yangjiefeng <yangjiefeng@gmail.com>
2026-08-16 01:35:03 +0800

    fix: import OpenAPI parameter annotation

--- spring-ai-rag-core/src/main/java/com/springairag/core/controller/RagSearchController.java
+++ spring-ai-rag-core/src/main/java/com/springairag/core/controller/RagSearchController.java
@@ -14,6 +14,7 @@ import com.springairag.core.service.CollectionIdentityResolver;
 import com.springairag.core.versioning.ApiVersion;
 import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
